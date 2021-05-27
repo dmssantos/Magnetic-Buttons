88cb78282ca774924c9fa31e1a577d1e260a9213
dmssantos <dms.santos_@hotmail.com>
2021-05-27 17:30:27 -0300

    Final commit

---
+++ script.js
@@ -0,0 +1,18 @@
+
+const btns = document.querySelectorAll(".btn")
+
+btns.forEach((btn) => {
+  btn.addEventListener("mousemove", function(e){
+    const position = btn.getBoundingClientRect()
+    const x = e.pageX - position.left - position.width / 2
+    const y = e.pageY - position.top - position.height / 2
+
+    btn.children[0].style.transform = "translate(" + x * 0.3 + "px, " + y * 0.5 + "px)"
+  })
+})
+
+btns.forEach((btn) => {
+  btn.addEventListener("mouseout", function(e){
+    btn.children[0].style.transform = "translate(0px, 0px)"
+  })
+})
